3e4f9345d1f33feb382f4e1bcec2b270aa8b6550
andresusanto <andre@andresusanto.info>
2015-10-19 19:33:44 +0700

    Stack implemented

--- src/groupdata/GroupData.java
+++ src/groupdata/GroupData.java
@@ -6,6 +6,8 @@
 
 package groupdata;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Andre
@@ -15,8 +17,43 @@ public class GroupData {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception{
+        String command;
+        Scanner scn = new Scanner(System.in);
+        ReplStack<String> replstack = new ReplStack<>("stack");
+        
+        System.out.println("DISTRIUTED STACK TESTING TOOL");
+        
+        while( !(command = scn.next()).equals("/exit") ) {
+            if (command.equals("/push")){
+                replstack.push(scn.next());
+            }else if (command.equals("/pop")){
+                if (replstack.size() == 0){
+                    System.out.println("Empty Stack");
+                }else{
+                    System.out.println(replstack.pop());
+                }
+            }else if (command.equals("/top")){
+                if (replstack.size() == 0){
+                    System.out.println("Empty Stack");
+                }else{
+                    System.out.println(replstack.top());
+                }
+                
+            }else if (command.equals("/empty")){
+                if (replstack.size() == 0){
+                    System.out.println("Empty Stack");
+                }else{
+                    while (replstack.size() > 0){
+                        System.out.println(replstack.pop());
+                    }
+                }
+            }else{
+                System.out.println("Invalid command!");
+            }
+        }
+        replstack.finish();
+        System.out.println("Bye bye!");
     }
     
 }
---
+++ src/groupdata/ReplStack.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package groupdata;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.Stack;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.util.Util;
+
+/**
+ *
+ * @author Andre
+ */
+public class ReplStack<T> extends ReceiverAdapter{
+    private JChannel channel;
+    private Stack stack;
+    
+    private static class DataMessage implements Serializable{
+        public int command;
+        public Object content;
+        
+        DataMessage(int cmd, Object obj){
+            command = cmd;
+            content = obj;
+        }
+    }
+    
+    ReplStack(String channelstr) throws Exception{
+        stack = new Stack<T>();
+        channel = new JChannel();
+        channel.setReceiver(this);
+        channel.connect(channelstr);
+        channel.getState(null, 10000);
+    }
+    
+    public void finish(){
+        channel.close();
+    }
+    
+    public void push(T obj) throws Exception{
+        stack.add(obj);
+        
+        DataMessage msgdata = new DataMessage(1, obj);
+        Message msg = new Message(null, null, msgdata);
+        channel.send(msg);
+    }
+    
+    public int size(){
+        return stack.size();
+    }
+    
+    public T pop() throws Exception{
+        
+        DataMessage msgdata = new DataMessage(2, null);
+        Message msg = new Message(null, null, msgdata);
+        channel.send(msg);
+        
+        return (T)stack.pop();
+    }
+    
+    public T top(){
+        return (T)stack.peek();
+    }
+    
+    public void receive(Message msg) {
+        if (msg.getSrc().equals(channel.getAddress())) return;
+        
+        DataMessage msgdata = (DataMessage) msg.getObject();
+        synchronized(stack) {
+            switch (msgdata.command){
+                case 1: //push
+                    stack.add(msgdata.content);
+                    break;
+                case 2: //pop
+                    stack.pop();
+                    break;
+            }
+        }
+    }
+    
+    public void getState(OutputStream output) throws Exception {
+        synchronized(stack) {
+            Util.objectToStream(stack, new DataOutputStream(output));
+        }
+    }
+    
+    public void setState(InputStream input) throws Exception {
+        Stack<T> list;
+        list=(Stack<T>)Util.objectFromStream(new DataInputStream(input));
+        
+        synchronized(stack) {
+            stack.clear();
+            stack.addAll(list);
+        }
+        
+        System.out.println("Stack synchronized");
+    }
+}
